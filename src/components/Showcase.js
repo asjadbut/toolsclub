f7b6e9e92360a8bad4a82b0fb4d8688eab3c9465
Asjad Butt <asjadb18@gmail.com>
2022-12-10 21:37:29 +0500

    initial commit

---
+++ src/components/Showcase.js
@@ -0,0 +1,21 @@
+import React from "react"
+function Showcase() {
+    return (
+        <section
+            className="bg-dark text-light p-5 p-lg-0 pt-lg-5 text-center text-sm-start"
+            style={{marginTop:'30px',height:'300px'}}
+        >
+            <div className="container">
+                <div className=" align-items-center justify-content-between">
+                    <div>
+                        <h1 style={{marginTop:'50px'}}>Free Online<span className="text-warning"> Converter Tools </span></h1>
+                        <p className="lead my-4">
+                        Convert media files online from one format into another. Please select the tool below as per your requirement
+                        </p>
+                    </div>
+                </div>
+            </div>
+         </section>
+    )
+}
+export default Showcase
